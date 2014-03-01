04e107fead6761bd344b417814f7761b2c27638e
ParkSeokJae <molla4455@naver.com>
2014-03-01 21:01:42 +0900

    variables.php

--- variables.php
+++ variables.php
@@ -1,15 +1,38 @@
 <html>
  <body>
 	<?php
-	/* need comment */
-		$a=1;
-		echo $a+1; #2
-		echo "<br />";
-		$a=2;
-		print $a+1; #3
-		echo "<br />";
-		$first = "coding";
-		echo $first." is easy.";
+	/* normal variable */
+	$a=1;
+	echo $a+1; #2
+	echo "<br />";
+	$a=2;
+	print $a+1; #3
+	echo "<br />";
+	$first = "coding";
+	echo $first." is easy.";
+
+	echo "<br />";
+	/* const variable */
+	define('TITLE', 'PHP Tutorial');
+	echo TITLE;
+
+	/* error occur 
+	define('TITLE', 'JAVA Tutorial');
+	echo TITLE;
+	*/
+
+	echo "<br />";
+	/* gettype, settype */
+	$a = 100;
+	echo gettype($a);
+	settype($a, 'double');
+	echo ' converted to '.gettype($a);
+	
+	echo "<br />";
+	/* variable variables */
+	$title = 'subject';
+	$$title = 'PHP Tutorial';
+	echo $subject;
 	?>
  </body>
 </html>
