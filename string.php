7ffb1bbd757df4f5954143b9a65cfbd87289155a
ParkSeokJae <molla4455@naver.com>
2014-03-01 20:49:16 +0900

    Signed-off-by: ParkSeokJae <molla4455@naver.com> aa

---
+++ string.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<?php
+echo var_dump(1234);
+echo var_dump("1234");
+echo "hello"." "."world";
+echo "he said, \"hi!\"";
+echo 'he said, "hi"';
+echo "¾È³çÇÏ¼¼¿ä";
+?>
---
+++ variables.php
@@ -0,0 +1,15 @@
+<html>
+ <body>
+	<?php
+	/* need comment */
+		$a=1;
+		echo $a+1; #2
+		echo "<br />";
+		$a=2;
+		print $a+1; #3
+		echo "<br />";
+		$first = "coding";
+		echo $first." is easy.";
+	?>
+ </body>
+</html>
