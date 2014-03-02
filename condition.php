02c335aabe585f71ad17fb1dbffac174f19d598e
ParkSeokJae <molla4455@naver.com>
2014-03-02 15:47:35 +0900

    condition - added if2

--- condition.php
+++ condition.php
@@ -1,9 +1,32 @@
 <?php
-# if
+# if-1
+/*
 if (false) {
 	echo "result : 1";
 } else {
 	echo "result : 2";
 }
+*/
+
+#if-2
+if (!true and !true){    
+	echo 1; #false
+}
+echo "<br />";
+
+if (!true and !false){    
+	echo 2; #false
+}
+echo "<br />";
+
+if (!false and !true){    
+	echo 3; #false
+}
+echo "<br />";
+
+if (!false and !false){
+    echo 4; #true!
+}
+echo "<br />";
 
 ?>
