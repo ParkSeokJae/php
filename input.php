70e739df828b7d03ebd4d88e88dc72317f54a1d2
ParkSeokJae <molla4455@naver.com>
2014-03-03 10:59:19 +0900

    input modify

--- input.php
+++ input.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8"/>
     </head>   
     <body>
-        <form action="./process.php?mode=insert" method="POST">
+        <form action="./process2.php?mode=insert" method="POST">
             <p>title : <input type="text" name="title"></p>
             <p>description : <textarea name="description" id="" cols="30" rows="10"></textarea></p>
             <p><input type="submit" /></p>            
